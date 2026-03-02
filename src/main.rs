e5131e280ada7708b126cc9ddafc00ef103997d0
Nick <nicholasakho@gmail.com>
2026-03-02 22:53:27 +1300

    Changed for loop to use usize

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,25 @@
 fn main() {
-
+    let game = Grid {size: 3, board: Vec::new()};
+    game.check_goal_state();
 }
 
 struct Grid {
-    size: i32, board: Vec<Vec<i32>>
+    size: u32, board: Vec<Vec<i32>>
 }
 
 impl Grid {
     fn check_goal_state(&self) -> bool {
         let counter: i32 = 0;
-        let goal_state : Vec<Vec<i32>>;
-        for i in 0..=((self.size**2) - 1) {
-            goal_state[()]
+        let mut goal_state : Vec<Vec<i32>> = Vec::new();
+        let n: usize = (self.size * self.size) as usize;
+        for i in 0..=n {
+            goal_state[i / 3][i % 3] = counter
         }
+        println!("{:?}", goal_state);
         return false
     }
     fn initialize_board() {
-        let board = vec![
+        let _board = vec![
             vec![0, 0, 0],
             vec![0, 0, 0],
             vec![0, 0, 0],
