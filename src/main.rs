57205f7f74c689baa2a1a343351a0f3d9c7ce907
Nick <nicholasakho@gmail.com>
2026-03-02 22:13:11 +1300

    Initial Grid class for N-Puzzle

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+fn main() {
+
+}
+
+struct Grid {
+    size: i32, board: Vec<Vec<i32>>
+}
+
+impl Grid {
+    fn check_goal_state(&self) -> bool {
+        let counter: i32 = 0;
+        let goal_state : Vec<Vec<i32>>;
+        for i in 0..=((self.size**2) - 1) {
+            goal_state[()]
+        }
+        return false
+    }
+    fn initialize_board() {
+        let board = vec![
+            vec![0, 0, 0],
+            vec![0, 0, 0],
+            vec![0, 0, 0],
+        ];
+    }
+}
